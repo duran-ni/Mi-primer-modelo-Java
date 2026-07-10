2d891dbe12dbdea60760efcab66ecac47b363497
duran-ni <duran_ni_7@hotmail.com>
2026-07-10 17:10:18 +0200

    test: add PersonTest with constructor and calculateAge coverage

---
+++ src/test/java/com/javaproject/person/PersonTest.java
@@ -0,0 +1,34 @@
+package com.javaproject.person;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.Year;
+
+class PersonTest {
+    @Test
+    void constructorInitializesAttributes() {
+        Person person = new Person("Ana", "Garcia", "12345678A", 1995);
+
+        assertEquals("Ana", person.getName());
+        assertEquals("Garcia", person.getLastName());
+        assertEquals("12345678A", person.getDocumentNumber());
+        assertEquals(1995, person.getBirthYear());
+    }
+
+    @Test
+    void calculateAgeReturnsCorrectAge() {
+        Person person = new Person("Carlos", "Lopez", "87654321B", 2000);
+        int expectedAge = Year.now().getValue() - 2000;
+
+        assertEquals(expectedAge, person.calculateAge());
+    }
+
+    @Test
+    void calculateAgeWithRecentBirthYear() {
+        Person person = new Person("Lucia", "Martin", "11111111C", 2010);
+        int expectedAge = Year.now().getValue() - 2010;
+
+        assertEquals(expectedAge, person.calculateAge());
+    }
+}
+
